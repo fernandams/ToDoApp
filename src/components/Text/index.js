589543b12b77a5cc59c7cd3200a5e79b1acf2a39
fernandams <nanda.sousa.m@gmail.com>
2020-10-31 04:09:35 -0300

    :lipstick: Updates the UI and style files
    Updated the UI of title and style files of project

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -1,10 +1,8 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  title: {
+    fontSize: 28,
+    color: "#8134AF",
   },
 });
